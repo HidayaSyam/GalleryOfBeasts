60ffeea2fd19e629363cf6ea1f4714e8bc2072b1
HidayaSyam <Hid.sy1910@gmail.com>
2021-07-01 00:23:30 +0300

    Class02

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import logo from '../logo.svg';
 
 class Header extends React.Component {
     render() {
         return (
-            <header>
-                Hello from Header
-            </header>
+            <>
+                <Navbar bg="dark" variant="dark">
+                    <Navbar.Brand href="#home">
+                        <img
+                            src={logo}
+                            alt=""
+                            className="d-inline-block align-top"
+                        />
+                        React Bootstrap
+                    </Navbar.Brand>
+                </Navbar>
+            </>
         )
     }
 }
--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,12 +1,25 @@
 import React from 'react'
 
 class HornedBeasts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            numberOfVotes: 0,
+        }
+
+    }
+    increaseVote= () =>{
+        this.setState({
+            numberOfVotes:this.state.numberOfVotes + 1,
+        })
+    }
     render() {
         return(
-            <div>
-                <img src={this.props.imageURL} />
+            <div > 
+                <img onClick={this.increaseVote} src={this.props.imageURL} alt={this.props.title} />
                 <h2> {this.props.title}</h2>
                 <p>{this.props.description}</p>
+                <h3>Votes : {this.state.numberOfVotes}</h3>
             </div>
         )
     }
